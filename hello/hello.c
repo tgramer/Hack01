94aa8e978346dc0777b97f4ea7a62a865989f6d6
Teresa Marie Gramer <tgramer>
2019-01-17 18:40:58 -0600

    Create hello.c

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Teresa Gramer and Andres Ruiz
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Teresa and Andres\n");
+
+  return 0;
+}
